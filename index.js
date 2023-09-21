6e2327cacdb8bbccec99302419ee0a0e7f9050fe
kimtaeyoung <trust00005@naver.com>
2023-09-21 12:38:20 +0900

    2023dev

---
+++ index.js
@@ -0,0 +1,6 @@
+var button = ducument.getElementById("move");
+        button.addEventListener("click", function() {
+            var newURL= "https://www.example.com";
+            window.location.href=newURL;
+        });
+  
